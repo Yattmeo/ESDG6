a3c1b176b676746fb0a9202f57caf252e1e30930
liangjunnnnn <ljlau.2022@scis.smu.edu.sg>
2024-03-22 12:15:34 +0800

    update ehrsql

--- database/ehr.sql
+++ database/ehr.sql
@@ -12,28 +12,20 @@ SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `EHR`
---
+-- Create the EHR database if not exists
 CREATE DATABASE IF NOT EXISTS `EHR` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `EHR`;
 
--- --------------------------------------------------------
-
---
 -- Table structure for table `ehr`
---
-
 DROP TABLE IF EXISTS `ehr`;
 CREATE TABLE IF NOT EXISTS `ehr` (
   `patient_id` int NOT NULL,
-  `medical_conditions` (varchar(255)),
+  `medical_conditions` varchar(255),
   `allergies` varchar(255),
   `medications` varchar(255),
   `surgeries` varchar(255),
@@ -43,14 +35,12 @@ CREATE TABLE IF NOT EXISTS `ehr` (
   PRIMARY KEY (`patient_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `ehr`
---
-
+-- Insert data into the `ehr` table
 INSERT INTO `ehr` (`patient_id`, `medical_conditions`, `allergies`, `medications`, `surgeries`, `immunisations`, `family_medical_history`, `past_appointments`) VALUES
-(1, 'Hypertension', 'Penicillin', 'Lisinopril', 'Appendectomy', 'Flu Vaccine', 'Heart disease in family', '["2023-01-15T10:30:00Z", "2022-11-10T08:45:00Z"]'),
-    (2, 'Diabetes', NULL, 'Metformin', NULL, NULL, NULL, '["2022-12-20T09:15:00Z", "2023-02-28T14:00:00Z"]'),
-    (3, 'Asthma', '["Aspirin", "Dust"]', 'Albuterol', 'Tonsillectomy', 'Tdap Vaccine', 'Asthma in family', '["2023-03-05T11:00:00Z", "2022-10-02T12:30:00Z"]');
+    (1, 'Hypertension', 'Penicillin', 'Lisinopril', 'Appendectomy', 'Flu Vaccine', 'Heart disease in family', '2023-01-15T10:30:00Z'),
+    (2, 'Diabetes', NULL, 'Metformin', NULL, NULL, NULL, '2022-12-20T09:15:00Z'),
+    (3, 'Asthma', '["Aspirin", "Dust"]', 'Albuterol', 'Tonsillectomy', 'Tdap Vaccine', 'Asthma in family', '2023-03-05T11:00:00Z');
+
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
