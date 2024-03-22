e8b178415083177dfe2a03fd126a25b3e06190eb
liangjunnnnn <ljlau.2022@scis.smu.edu.sg>
2024-03-22 21:08:48 +0800

    uploaded the other two DBs from drive, but import failed when i put in sql

---
+++ database/appointmentDB.sql
@@ -0,0 +1,77 @@
+-- ---
+-- Globals
+-- ---
+
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- ---
+-- Table 'Doctors'
+-- 
+-- ---
+CREATE DATABASE IF NOT EXISTS `appointmentDB` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `appointmentDB`;
+
+DROP TABLE IF EXISTS `Doctors`;
+		
+CREATE TABLE `Doctors` (
+  `DoctorID` INTEGER NOT NULL AUTO_INCREMENT DEFAULT NULL,
+  `Specialty` VARCHAR(255) NULL DEFAULT NULL,
+  `Name` VARCHAR(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`DoctorID`)
+);
+
+-- ---
+-- Table 'Patients'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `Patients`;
+		
+CREATE TABLE `Patients` (
+  `PatientID` INTEGER NOT NULL AUTO_INCREMENT DEFAULT NULL,
+  `Name` VARCHAR(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`PatientID`)
+);
+
+-- ---
+-- Table 'Appointments'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `Appointments`;
+		
+CREATE TABLE `Appointments` (
+  `AppointmentID` INTEGER NOT NULL AUTO_INCREMENT DEFAULT NULL,
+  `Datetime` DATETIME NOT NULL DEFAULT NULL,
+  `Clinic` INTEGER NOT NULL DEFAULT NULL,
+  `DoctorID_Doctors` INTEGER NOT NULL DEFAULT NULL,
+  `PatientID_Patients` INTEGER NOT NULL DEFAULT NULL,
+  PRIMARY KEY (`AppointmentID`)
+);
+
+-- ---
+-- Foreign Keys 
+-- ---
+
+ALTER TABLE `Appointments` ADD FOREIGN KEY (DoctorID_Doctors) REFERENCES `Doctors` (`DoctorID`);
+ALTER TABLE `Appointments` ADD FOREIGN KEY (PatientID_Patients) REFERENCES `Patients` (`PatientID`);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `Doctors` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `Patients` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `Appointments` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `Doctors` (`DoctorID`,`Specialty`,`Name`) VALUES
+-- ('','','');
+-- INSERT INTO `Patients` (`PatientID`,`Name`) VALUES
+-- ('','');
+-- INSERT INTO `Appointments` (`AppointmentID`,`Datetime`,`Clinic`,`DoctorID_Doctors`,`PatientID_Patients`) VALUES
+-- ('','','','','');
---
+++ database/schedulingDB.sql
@@ -0,0 +1,49 @@
+-- ---
+-- Globals
+-- ---
+
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- ---
+-- Table 'DoctorAvailability'
+-- 
+-- ---
+CREATE DATABASE IF NOT EXISTS `schedulingDB` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `schedulingDB`;
+
+DROP TABLE IF EXISTS `DoctorAvailability`;
+
+CREATE TABLE `DoctorAvailability` (
+  `AppointmentID` INTEGER NOT NULL AUTO_INCREMENT,
+  `Date` DATE NOT NULL,
+  `SlotNum` TINYINT NOT NULL,
+  `DoctorID_Doctors` INT NOT NULL,
+  PRIMARY KEY (`AppointmentID`),
+  FOREIGN KEY (`DoctorID_Doctors`) REFERENCES `Doctors` (`DoctorID`)
+);
+
+DROP TABLE IF EXISTS `Doctors`;
+
+CREATE TABLE `Doctors` (
+  `DoctorID` INTEGER NOT NULL AUTO_INCREMENT,
+  `Specialty` VARCHAR(255),
+  `Name` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`DoctorID`)
+);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `DoctorAvailability` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `Doctors` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `DoctorAvailability` (`AppointmentID`,`Date`,`SlotNum`,`DoctorID_Doctors`) VALUES
+-- ('','','','');
+-- INSERT INTO `Doctors` (`DoctorID`,`Specialty`,`Name`) VALUES
+-- ('','','');
