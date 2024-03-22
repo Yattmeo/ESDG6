10c91f0a7a15d0a65ca21bac9442c97776f67bfd
yourlaststraw <vnivethikaa@gmail.com>
2024-03-22 10:21:41 +0800

    Update sql

--- database/userdetails.sql
+++ database/userdetails.sql
@@ -1,20 +1,54 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jan 14, 2019 at 06:42 AM
+-- Server version: 5.7.19
+-- PHP Version: 7.1.9
 
-CREATE DATABASE USERDETAILS;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-USE USERDETAILS;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Database: `USERDETAILS`
+--
+CREATE DATABASE IF NOT EXISTS `USERDETAILS` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `USERDETAILS`;
 
-CREATE TABLE user_details (
-    User_ID INT PRIMARY KEY,
-    phone_number BIGINT,
-    email VARCHAR(255),
-    address VARCHAR(255)
-);
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `user_details`
+--
 
-INSERT INTO user_details (User_ID, phone_number, email, address)
-VALUES
-    (1, 1234567890, 'john@example.com', '123 Main Street'),
-    (2, 9876543210, 'jane@example.com', '456 Elm Street'),
-    (3, 5551234567, 'alice@example.com', '789 Oak Avenue');
+DROP TABLE IF EXISTS `user_details`;
+CREATE TABLE IF NOT EXISTS `user_details` (
+  `User_ID` int(11) NOT NULL,
+  `phone_number` bigint(20) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  PRIMARY KEY (`User_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user_details`
+--
+
+INSERT INTO `user_details` (`User_ID`, `phone_number`, `email`, `address`) VALUES
+(1, 1234567890, 'john@example.com', '123 Main Street'),
+(2, 9876543210, 'jane@example.com', '456 Elm Street'),
+(3, 5551234567, 'alice@example.com', '789 Oak Avenue');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
