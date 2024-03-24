d383d4da410741ea4b55feaee301268bcd49d6d0
mriyas830 <136709016+mriyas830@users.noreply.github.com>
2024-03-25 00:21:01 +0800

    Update updateavailability.py

--- project/services/Simple/updateavailability.py
+++ project/services/Simple/updateavailability.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 from sqlalchemy.orm import relationship
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL') or 'mysql+mysqlconnector://root@localhost:8889/schedulingDB'
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL') or 'mysql+mysqlconnector://root@localhost:3306/schedulingDB'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_recycle': 299}
 
