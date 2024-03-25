3de59faf207cefac023e968be4cf4ff8996396e0
yourlaststraw <vnivethikaa@gmail.com>
2024-03-25 11:36:26 +0800

    Update schedulingDB.sql

--- database/schedulingDB.sql
+++ database/schedulingDB.sql
@@ -1,21 +1,16 @@
--- ---
 -- Globals
--- ---
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET FOREIGN_KEY_CHECKS=0;
 
--- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
--- SET FOREIGN_KEY_CHECKS=0;
-
--- ---
--- Table 'DoctorAvailability'
--- 
--- ---
+-- Create database if not exists
 CREATE DATABASE IF NOT EXISTS `schedulingDB` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `schedulingDB`;
 
+-- Drop existing tables if they exist
 DROP TABLE IF EXISTS `DoctorAvailability`;
-
 DROP TABLE IF EXISTS `Doctors`;
 
+-- Create Doctors table
 CREATE TABLE `Doctors` (
   `DoctorID` INTEGER NOT NULL AUTO_INCREMENT,
   `Specialty` VARCHAR(255),
@@ -23,6 +18,7 @@ CREATE TABLE `Doctors` (
   PRIMARY KEY (`DoctorID`)
 );
 
+-- Create DoctorAvailability table
 CREATE TABLE `DoctorAvailability` (
   `AppointmentID` INTEGER NOT NULL AUTO_INCREMENT,
   `Date` DATE NOT NULL,
@@ -32,19 +28,16 @@ CREATE TABLE `DoctorAvailability` (
   FOREIGN KEY (`DoctorID_Doctors`) REFERENCES `Doctors` (`DoctorID`)
 );
 
+-- Insert dummy data into Doctors table
+INSERT INTO `Doctors` (`Specialty`, `Name`) VALUES
+('Cardiologist', 'Dr. Smith'),
+('Neurologist', 'Dr. Johnson'),
+('Pediatrician', 'Dr. Williams'),
+('Dermatologist', 'Dr. Brown');
 
--- ---
--- Table Properties
--- ---
-
--- ALTER TABLE `DoctorAvailability` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `Doctors` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
--- ---
--- Test Data
--- ---
-
--- INSERT INTO `DoctorAvailability` (`AppointmentID`,`Date`,`SlotNum`,`DoctorID_Doctors`) VALUES
--- ('','','','');
--- INSERT INTO `Doctors` (`DoctorID`,`Specialty`,`Name`) VALUES
--- ('','','');
+-- Insert dummy data into DoctorAvailability table
+INSERT INTO `DoctorAvailability` (`Date`, `SlotNum`, `DoctorID_Doctors`) VALUES
+('2024-03-25', 1, 1),
+('2024-03-25', 2, 2),
+('2024-03-26', 1, 3),
+('2024-03-26', 2, 4);
