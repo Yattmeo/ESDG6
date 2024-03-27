60db8e7fa0a425c06e38a9f9bf1b09d46f6cf6ab
mriyas830 <136709016+mriyas830@users.noreply.github.com>
2024-03-27 12:42:10 +0800

    Update updateavailability.py

--- project/services/Simple/updateavailability.py
+++ project/services/Simple/updateavailability.py
@@ -65,4 +65,4 @@ def get_availability(doctorID):
         return jsonify({"responseStatus": "fail", "dateItems": []})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000,debug=True)
